9bca11b7506a5748f362e0af46f0d023eb65db46
Chems Sai <chems.sai@gmail.com>
2020-08-04 19:26:13 +0100

    changed style

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,5 +1,7 @@
+// const { STATUS_CODES } = require("http");
+
 $("#recipeCarousel").carousel({
-  interval: 5000,
+  interval: 100000,
 });
 
 $(".carousel .carousel-item").each(function () {
@@ -19,14 +21,42 @@ $(".carousel .carousel-item").each(function () {
     next.children(":first-child").clone().appendTo($(this));
   }
 });
-// Carousel update code
-// var item1;
 
-// document.getElementById("carousel-prev-link").onclick = newTitle;
-// function newTitle() {
-//   item1 = prompt("Enter a new first thing: ");
-//   updateList();
-// }
-// function updateList() {
-//   document.getElementById("carousel-card1-title").innerHTML = item1;
-// }
+// //  Change hero area img
+// $(document).ready(function () {
+//   var urls = [
+//     "../assets/img/road-1.jpg",
+//     "../assets/img/road-2.jpg",
+//     "../assets/img/road-3.jpg",
+//     "../assets/img/road-4.jpg",
+//   ];
+
+//   var cout = 1;
+
+//   $(".hero-area").css("background-image", 'url("' + urls[0] + '")');
+//   setInterval(function () {
+//     $(".hero-area").css("background-image", 'url("' + urls[cout] + '")');
+//     cout == urls.length - 1 ? (cout = 0) : cout++;
+//   }, 1000);
+// });
+
+// Good Code
+
+// app.use(express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/sprites'));
+
+// to be used with
+// <svg style="color: red;">
+//     <use xlink:href="solid.svg#ad"></use>
+// </svg>
+
+// // Carousel update code
+// // var item1;
+
+// // document.getElementById("carousel-prev-link").onclick = newTitle;
+// // function newTitle() {
+// //   item1 = prompt("Enter a new first thing: ");
+// //   updateList();
+// // }
+// // function updateList() {
+// //   document.getElementById("carousel-card1-title").innerHTML = item1;
+// // }
